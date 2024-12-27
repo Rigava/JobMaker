f8c34681068bea17451ead04e09774101ef5b3f5
NetZero <73232007+Rigava@users.noreply.github.com>
2024-12-27 21:04:07 +0530

    Update cv_recommend.py

--- cv_recommend.py
+++ cv_recommend.py
@@ -12,8 +12,8 @@ from langchain_community.document_loaders import WebBaseLoader
 
 # Toggle to the secret keys when deploying in streamlit community
 
-groq_key =st.secrets.API_KEY
-# groq_key = "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"
+# groq_key =st.secrets.API_KEY
+groq_key = "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"
 
 
 # main functions
